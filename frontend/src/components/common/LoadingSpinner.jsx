9843f9f0cb8245efb0a2be71b381b1aebe759ffd
dawit2123 <dawitzewdu2123@gmail.com>
2024-06-26 14:57:49 +0300

    Revert "project phase started"
    This reverts commit 10c24b367e396ef719a1c1849284f5ecee0bbb3f.

--- frontend/src/components/common/LoadingSpinner.jsx
+++
@@ -1,6 +0,0 @@
-const LoadingSpinner = ({ size = "md" }) => {
-	const sizeClass = `loading-${size}`;
-
-	return <span className={`loading loading-spinner ${sizeClass}`} />;
-};
-export default LoadingSpinner;
