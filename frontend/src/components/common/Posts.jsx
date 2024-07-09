a9b1a920a07e5e0068fab9cf2133756454914ee0
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-09 15:58:32 +0300

    chore: Refactor Posts component to improve code readability and maintainability

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,70 +4,72 @@ import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 const Posts = ({ feedType, username, userId }) => {
-	const getPostEndpoint = () => {
-		switch (feedType) {
-			case "forYou":
-				return "/api/posts/all";
-			case "following":
-				return "/api/posts/following";
-			case "posts":
-				return `/api/posts/user/${username}`;
-			case "likes":
-				return `/api/posts/likes/${userId}`;
-			default:
-				return "/api/posts/all";
-		}
-	};
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/posts/all";
+      case "following":
+        return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+      case "likes":
+        return `/api/posts/likes/${userId}`;
+      default:
+        return "/api/posts/all";
+    }
+  };
 
-	const POST_ENDPOINT = getPostEndpoint();
+  const POST_ENDPOINT = getPostEndpoint();
 
-	const {
-		data: posts,
-		isLoading,
-		refetch,
-		isRefetching,
-	} = useQuery({
-		queryKey: ["posts"],
-		queryFn: async () => {
-			try {
-				const res = await fetch(POST_ENDPOINT);
-				const data = await res.json();
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
 
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
-				}
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
 
-				return data;
-			} catch (error) {
-				throw new Error(error);
-			}
-		},
-	});
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
 
-	useEffect(() => {
-		refetch();
-	}, [feedType, refetch, username]);
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch, username]);
 
-	return (
-		<>
-			{(isLoading || isRefetching) && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && !isRefetching && posts?.length === 0 && (
-				<p className='text-center my-4'>No posts in this tab. Switch 👻</p>
-			)}
-			{!isLoading && !isRefetching && posts && (
-				<div>
-					{posts.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {(isLoading || isRefetching) && (
+        <div className="flex flex-col justify-center">
+          <PostSkeleton />
+          <PostSkeleton />
+          <PostSkeleton />
+        </div>
+      )}
+      {!isLoading && !isRefetching && posts?.length === 0 && (
+        <p className="text-center my-4">
+          No posts in this tab. Create your post.
+        </p>
+      )}
+      {!isLoading && !isRefetching && posts && (
+        <div>
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 export default Posts;
