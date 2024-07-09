3e9fba74961eb4a6e91b3609558c502c1f1073e5
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-09 16:12:28 +0300

    chore: Update login page logo text and file extension to use .png

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -69,7 +69,9 @@ const LoginPage = () => {
             alt="Afrinet"
             className="w-12 h-12 rounded-full fill-white"
           />{" "}
-          <h1 className="text-4xl font-extrabold text-white">{"Let's"} go.</h1>
+          <h1 className="text-4xl font-extrabold text-white">
+            Tech Newsletter
+          </h1>
           <label className="input input-bordered rounded flex items-center gap-2">
             <MdOutlineMail />
             <input
