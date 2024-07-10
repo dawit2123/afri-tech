3c52f0a6d94490d888ecc5d5fd4ef509f3dc6d80
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-10 21:52:15 +0300

    Refactor Sidebar component to update home icon and link text

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { MdHomeFilled } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
 import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -36,11 +36,11 @@ const Sidebar = () => {
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
       <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full">
-        <Link to="/" className="flex justify-center md:justify-start">
+        <Link to="/" className="flex justify-start md:justify-start">
           <img
             src={afrinet}
             alt="Afrinet"
-            className="w-12 h-12 rounded-full fill-white hover:bg-stone-900"
+            className="w-20 h-20 rounded-full fill-white hover:bg-stone-900"
           />
         </Link>
         <ul className="flex flex-col gap-3 mt-4">
@@ -49,8 +49,8 @@ const Sidebar = () => {
               to="/"
               className="flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer"
             >
-              <MdHomeFilled className="w-8 h-8" />
-              <span className="text-lg hidden md:block">Home</span>
+              <FaHome className="w-8 h-8" />
+              <span className="text-lg hidden md:block">Tech News</span>
             </Link>
           </li>
           <li className="flex justify-center md:justify-start">
