45c0fc443ae146b2d8bc4da12dc9bdc582448897
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-10 21:53:15 +0300

    Refactor HomePage component to update feed type options and improve code readability

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -4,13 +4,22 @@ import Posts from "../../components/common/Posts";
 import CreatePost from "./CreatePost";
 
 const HomePage = () => {
-  const [feedType, setFeedType] = useState("forYou");
+  const [feedType, setFeedType] = useState("following");
 
   return (
     <>
       <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
         {/* Header */}
         <div className="flex w-full border-b border-gray-700">
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("following")}
+          >
+            Tech News
+            {feedType === "following" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
           <div
             className={
               "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
@@ -22,19 +31,8 @@ const HomePage = () => {
               <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
             )}
           </div>
-          <div
-            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-            onClick={() => setFeedType("following")}
-          >
-            Tech News
-            {feedType === "following" && (
-              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
-            )}
-          </div>
         </div>
-
-        {/*  CREATE POST INPUT */}
-        <CreatePost />
+        {feedType === "following" ? "" : <CreatePost />}
 
         {/* POSTS */}
         <Posts feedType={feedType} />
