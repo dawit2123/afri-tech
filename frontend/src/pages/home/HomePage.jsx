83639e564fdc63536e2429df0f41809b2b3c76a0
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-10 16:52:57 +0300

    Refactor HomePage component to improve code readability and formatting

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -4,42 +4,42 @@ import Posts from "../../components/common/Posts";
 import CreatePost from "./CreatePost";
 
 const HomePage = () => {
-	const [feedType, setFeedType] = useState("forYou");
+  const [feedType, setFeedType] = useState("forYou");
 
-	return (
-		<>
-			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
-				{/* Header */}
-				<div className='flex w-full border-b border-gray-700'>
-					<div
-						className={
-							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-						}
-						onClick={() => setFeedType("forYou")}
-					>
-						For you
-						{feedType === "forYou" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-					<div
-						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
-						onClick={() => setFeedType("following")}
-					>
-						Following
-						{feedType === "following" && (
-							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
-						)}
-					</div>
-				</div>
+  return (
+    <>
+      <div className="flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen">
+        {/* Header */}
+        <div className="flex w-full border-b border-gray-700">
+          <div
+            className={
+              "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            }
+            onClick={() => setFeedType("forYou")}
+          >
+            Your tech posts
+            {feedType === "forYou" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+          <div
+            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            onClick={() => setFeedType("following")}
+          >
+            Tech News
+            {feedType === "following" && (
+              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
+            )}
+          </div>
+        </div>
 
-				{/*  CREATE POST INPUT */}
-				<CreatePost />
+        {/*  CREATE POST INPUT */}
+        <CreatePost />
 
-				{/* POSTS */}
-				<Posts feedType={feedType} />
-			</div>
-		</>
-	);
+        {/* POSTS */}
+        <Posts feedType={feedType} />
+      </div>
+    </>
+  );
 };
 export default HomePage;
