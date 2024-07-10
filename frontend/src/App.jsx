ac589f803fc80dc4f7b0b902673c2844eb20cb0a
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-10 15:09:42 +0300

    chore: Refactor App component to improve code readability and formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,49 +14,64 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-	const { data: authUser, isLoading } = useQuery({
-		// we use queryKey to give a unique name to our query and refer to it later
-		queryKey: ["authUser"],
-		queryFn: async () => {
-			try {
-				const res = await fetch("/api/auth/me");
-				const data = await res.json();
-				if (data.error) return null;
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
-				}
-				console.log("authUser is here:", data);
-				return data;
-			} catch (error) {
-				throw new Error(error);
-			}
-		},
-		retry: false,
-	});
+  const { data: authUser, isLoading } = useQuery({
+    // we use queryKey to give a unique name to our query and refer to it later
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) return null;
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        console.log("authUser is here:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
 
-	if (isLoading) {
-		return (
-			<div className='h-screen flex justify-center items-center'>
-				<LoadingSpinner size='lg' />
-			</div>
-		);
-	}
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
 
-	return (
-		<div className='flex max-w-6xl mx-auto'>
-			{/* Common component, bc it's not wrapped with Routes */}
-			{authUser && <Sidebar />}
-			<Routes>
-				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
-				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
-				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
-				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
-				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
-			</Routes>
-			{authUser && <RightPanel />}
-			<Toaster />
-		</div>
-	);
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      {/* Common component, bc it's not wrapped with Routes */}
+      {authUser && <Sidebar />}
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      {authUser && <RightPanel />}
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
