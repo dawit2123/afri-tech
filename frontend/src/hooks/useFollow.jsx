f51527aa466caaf405d21878b89e02533037a02d
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-15 12:18:45 +0300

    Refactor useFollow hook to improve code readability and formatting

--- frontend/src/hooks/useFollow.jsx
+++ frontend/src/hooks/useFollow.jsx
@@ -2,36 +2,35 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 const useFollow = () => {
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	const { mutate: follow, isPending } = useMutation({
-		mutationFn: async (userId) => {
-			try {
-				const res = await fetch(`/api/users/follow/${userId}`, {
-					method: "POST",
-				});
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async (userId) => {
+      try {
+        const res = await fetch(`/api/users/follow/${userId}`, {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong!");
+        }
+        return;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
-				const data = await res.json();
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong!");
-				}
-				return;
-			} catch (error) {
-				throw new Error(error.message);
-			}
-		},
-		onSuccess: () => {
-			Promise.all([
-				queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
-				queryClient.invalidateQueries({ queryKey: ["authUser"] }),
-			]);
-		},
-		onError: (error) => {
-			toast.error(error.message);
-		},
-	});
-
-	return { follow, isPending };
+  return { follow, isPending };
 };
 
 export default useFollow;
