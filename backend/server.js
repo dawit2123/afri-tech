7c94318338148f2f98f548d76f96647221f67f1b
dawit2123 <dawitzewdu2123@gmail.com>
2024-07-30 22:28:00 +0300

    code refactored

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,6 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
